0384f32e59b37851c410531ee4d2b53cc8e741c6
misiucodes <meeshtran15@gmail.com>
2022-02-28 15:44:21 -0500

    Created Synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
-  return (
-    <div className="meaning-container">
-      <h2 className="partOfSpeech">{props.meaning.partOfSpeech}</h2>
-      
-      {props.meaning.definitions.map(function(definition, index) {
-        return (
-          <div className="definitions" key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
-    </div>
+return (
+  <div className="meanings">
+    <h2>{props.meaning.partOfSpeech}</h2>
+
+    {props.meaning.definitions.map(function(definition, index) {
+      return (
+        <div key={index}>
+          <p>
+            <strong>Definition:</strong> {definition.definition}
+            <br />
+            <strong>Example:</strong>
+            <em> {definition.example}</em>
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
+          </p>
+        </div>
+      );
+    })}
+  </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -3,17 +3,16 @@ import Meaning from "./Meaning";
 import "./Results.css";
 
 export default function Results(props) {
-
-  console.log(props.results);
+  console.log(props.results); 
   if (props.results) {
   return (
     <div className="results-container">
-      <h1 className="word">{props.results.word}</h1>
-      {props.results.meanings.map(function(meaning, index) {
+      <h1>{props.results.word}</h1>
+      {props.results.meanings.map(function (meanings, index) {
         return (
-        <div key={index}>
-          <Meaning meaning={meaning} />
-        </div>
+          <div key={index}>
+            <Meaning meaning={meanings} />
+          </div>
         );
       })}
     </div>
